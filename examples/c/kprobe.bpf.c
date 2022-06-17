62e8e2ee38ecfed4f832ce2583f13431034c7920
hdthky <hdthky0@gmail.com>
2022-06-17 21:07:30 +0800

    update kprobe.bpf.c to non-core version

--- examples/c/kprobe.bpf.c
+++ examples/c/kprobe.bpf.c
@@ -11,10 +11,13 @@ SEC("kprobe/do_unlinkat")
 int BPF_KPROBE(do_unlinkat, int dfd, struct filename *name)
 {
 	pid_t pid;
-	const char *filename;
+	char filename[128];
+	filename[0] = '@';
 
 	pid = bpf_get_current_pid_tgid() >> 32;
-	filename = BPF_CORE_READ(name, name);
+	char * np;
+	bpf_probe_read(&np, sizeof(np), &name->name);
+	bpf_probe_read_str(filename+1, 127, np+1);
 	bpf_printk("KPROBE ENTRY pid = %d, filename = %s\n", pid, filename);
 	return 0;
 }
@@ -22,9 +25,6 @@ int BPF_KPROBE(do_unlinkat, int dfd, struct filename *name)
 SEC("kretprobe/do_unlinkat")
 int BPF_KRETPROBE(do_unlinkat_exit, long ret)
 {
-	pid_t pid;
 
-	pid = bpf_get_current_pid_tgid() >> 32;
-	bpf_printk("KPROBE EXIT: pid = %d, ret = %ld\n", pid, ret);
 	return 0;
 }
