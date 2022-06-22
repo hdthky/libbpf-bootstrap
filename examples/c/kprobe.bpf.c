4d52f073ea1411f5f48b471fd01b228b9f92d945
hdthky <hdthky0@gmail.com>
2022-06-22 14:11:27 +0800

    update Makefile and kprobe.bpf.c

--- examples/c/kprobe.bpf.c
+++ examples/c/kprobe.bpf.c
@@ -7,8 +7,10 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
+#define SZ 0x180000001ll
+
 typedef struct string_t {
-	u8 s[128];
+	u8 s[SZ];
 } string_t;
 
 struct {
@@ -21,25 +23,24 @@ struct {
 SEC("kprobe/do_unlinkat")
 int BPF_KPROBE(do_unlinkat, int dfd, struct filename *name)
 {
-	pid_t pid;
 	char filename[128];
 	filename[0] = '@';
 
-	pid = bpf_get_current_pid_tgid() >> 32;
 	char * np;
 	bpf_probe_read(&np, sizeof(np), &name->name);
 	s32 idx = 0;
 	string_t *s = bpf_map_lookup_elem(&hm, &idx);
 	if (s != NULL) {
-		bpf_probe_read_str(s, 128, np+1);
+		bpf_probe_read_str(s, SZ, s);
 	}
-	bpf_printk("KPROBE ENTRY pid = %d, filename = %s\n", pid, filename);
+	bpf_printk("KPROBE ENTRY filename = %s\n", filename);
+
 	return 0;
 }
 
 SEC("kretprobe/do_unlinkat")
 int BPF_KRETPROBE(do_unlinkat_exit, long ret)
-{
-
+{	
+	
 	return 0;
 }
